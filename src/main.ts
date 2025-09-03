4049e4dbd3a6e564a8ce348882000ddf8af5dfbc
Frenchfryegr <frenchfrycaleb@gmail.com>
2025-09-02 23:13:06 -0500

    change cors url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', 
+    // origin: 'http://localhost:4200', 
+    origin: 'https://lol-skins-api.onrender.com',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
